50b2086f476b28bba619aca47840d27ee2fa3825
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2024-11-19 02:06:37 +0300

    VKM-26: add all available panels in view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState, useEffect, ReactNode } from 'react';
 import bridge, { UserInfo } from '@vkontakte/vk-bridge';
-import { View, SplitLayout, SplitCol } from '@vkontakte/vkui';
-import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
+import {View, SplitLayout, SplitCol, Panel, Button} from '@vkontakte/vkui';
+import {useActiveVkuiLocation, useRouteNavigator} from '@vkontakte/vk-mini-apps-router';
 
 import { Home } from './panels';
-import { DEFAULT_VIEW_PANELS } from './routes';
+import {DEFAULT_VIEW, DEFAULT_VIEW_PANELS} from "./routes.ts";
+import {ChoosePattern} from "./panels/ChoosePattern.tsx";
+import {Resume} from "./panels/Resume.tsx";
 
 export const App = () => {
-  const { panel: activePanel = DEFAULT_VIEW_PANELS.HOME } = useActiveVkuiLocation();
+  const { view: activeView, panel: activePanel } = useActiveVkuiLocation();
   const [fetchedUser, setUser] = useState<UserInfo | undefined>();
   const [popout, setPopout] = useState<ReactNode | null>();
 
@@ -23,8 +25,16 @@ export const App = () => {
   return (
     <SplitLayout popout={popout}>
       <SplitCol>
-        <View activePanel={activePanel}>
-          <Home id="home" fetchedUser={fetchedUser} />
+        <View nav={DEFAULT_VIEW} activePanel={activePanel}>
+          <Panel nav={DEFAULT_VIEW_PANELS.HOME}>
+            <Home id="home" fetchedUser={fetchedUser} />
+          </Panel>
+          <Panel nav={DEFAULT_VIEW_PANELS.PATTERN}>
+            <ChoosePattern />
+          </Panel>
+          <Panel nav={DEFAULT_VIEW_PANELS.CREATE}>
+            <Resume id="resume" fetchedUser={fetchedUser} />
+          </Panel>
         </View>
       </SplitCol>
     </SplitLayout>
