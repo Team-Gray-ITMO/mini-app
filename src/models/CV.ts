eead3332bb7e83592310f471620806cd40469317
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2024-12-10 13:13:47 +0300

    VKM-54: change cv model types

--- src/models/CV.ts
+++ src/models/CV.ts
@@ -1,6 +1,8 @@
 import {ConnectionType} from "../enums/ConnectionType.ts";
 import {SpecialityDto} from "../api/internal/dto/SpecialityDto.ts";
 import {WorkFormatDto} from "../api/internal/dto/WorkFormatDto.ts";
+import {UniversityDto} from "../api/vk/dto/UniversityDto.ts";
+import {CareerDto} from "../api/vk/dto/CareerDto.ts";
 
 export class CV {
     constructor(
@@ -14,8 +16,8 @@ export class CV {
         public city: string,
         public isReadyToMove: boolean,
         public avatar: string,
-        public education: string,
-        public workExperience: string
+        public education: UniversityDto[],
+        public workExperience: CareerDto[]
     ) {
     }
 }
